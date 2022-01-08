c17dcd3338ed8feb6da3042074e49fb3462739c0
Gabriel Alves <ga.jaimealves@gmail.com>
2022-01-07 21:24:04 -0300

    iniciando camada api

---
+++ api_net5/Controllers/UserController.cs
@@ -0,0 +1,32 @@
+﻿using api_net5.Core.Exceptions;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api_net5.Controllers
+{
+    [Route("/api/v1")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [HttpPost]
+        [Route("/users/create")]
+        public async Task<IActionResult> Create()
+        {
+            try
+            {
+
+            }
+            catch (DomainException ex)
+            {
+                return BadRequest();
+            }
+            catch (Exception)
+            {
+                return StatusCode(500, "Erro");
+            }
+        }
+    }
+}
