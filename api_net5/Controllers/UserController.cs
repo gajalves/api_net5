7b26950aed2c48e4199b041404aad30ba9945bf7
Gabriel <gajalves@gmail.com>
2022-01-09 17:23:22 -0300

    wip

--- api_net5/Controllers/UserController.cs
+++ api_net5/Controllers/UserController.cs
@@ -1,4 +1,6 @@
 ﻿using api_net5.Core.Exceptions;
+using api_net5.Services.Interfaces;
+using api_net5.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -6,18 +8,19 @@ using System.Linq;
 using System.Threading.Tasks;
 
 namespace api_net5.Controllers
-{
-    [Route("/api/v1")]
+{    
     [ApiController]
     public class UserController : ControllerBase
     {
+        private readonly IUserService _userservice;
+
         [HttpPost]
-        [Route("/users/create")]
-        public async Task<IActionResult> Create()
+        [Route("/api/v1/users/create")]
+        public async Task<IActionResult> Create([FromBody]CreateUserViewModel userViewModel)
         {
             try
             {
-
+                return Ok();
             }
             catch (DomainException ex)
             {
